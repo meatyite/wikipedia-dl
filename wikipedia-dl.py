1a4f77fbfbc74bba42dea6191e66ddd6b2314f30
Amir Yosef Perehodnik <43294802+sl4vkek@users.noreply.github.com>
2020-08-28 16:02:55 +0300

    Add files via upload

---
+++ wikipedia-dl.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import requests
+from urllib.parse import unquote
+from sys import argv
+
+session = requests.Session()
+session.headers = {'User-Agent': 'wikipedia-dl test bot'}
+
+
+def download_pdf(url):
+    print("Parsing URL...")
+    url_split = url.split('/')
+    domain = url_split[2]
+    real_page_name = ' slash '.join(url_split[4:]).replace('_', ' ')
+    real_page_name = unquote(real_page_name)
+    page_name = real_page_name.replace(' slash ', '/')
+    print('Article {} on website {}'.format(real_page_name, url_split[0] + '//' + domain))
+    print('Sending request...')
+    pdf = session.post(
+        url_split[0] + '//' + domain + '/wiki/Special:DownloadAsPdf',
+        data={
+            'page': page_name,
+            'action': 'redirect-to-electron'
+        }
+    )
+    file_name = real_page_name.strip() + '.pdf'
+    print('PDF Received. Writing to file {}...'.format(file_name))
+    open(file_name, 'wb').write(pdf.content)
+    print('Done!')
+
+
+if __name__ == '__main__':
+    if len(argv) >= 1:
+        download_pdf(argv[1])
